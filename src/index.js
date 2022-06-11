5273b8dfc28a94d4dae65cdcd315bfd1e1ed19b9
Ben Casson <bcasson11@gmail.com>
2022-06-11 09:17:22 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './reset.css';
+// import './assets/reset.css';
+import './assets/style.scss';
+import { themeButton } from './theme';
+
+themeButton.addEventListener('click', () => {
+    document.body.className ==='dark' ? document.body.className ='light' 
+                                      : document.body.className ='dark';
+});
+//on page load - display instructions if local storage is empty
--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,6 @@
-const setTheme = theme => document.documentElement.className = theme;
+// export const setTheme = theme => document.documentElement.className = theme;
+
+export const themeButton = document.getElementById("theme-button");
+// themeButton.addEventListener('click', () => {
+//     document.body.className ==='dark' ? document.body.className ='light' : document.body.className ='dark';
+// });
