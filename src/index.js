dd899779261ccf1f7c4fce1fa7be1223a01a3f56
Ben Casson <bcasson11@gmail.com>
2022-06-11 19:23:22 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ themeButton.addEventListener('click', () => {
     document.body.className ==='dark' ? document.body.className ='light' 
                                       : document.body.className ='dark';
 });
+
+const siteContainer = document.getElementById("site-container");
+siteContainer.style.height = window.innerHeight + "px";
+window.addEventListener('resize', () => {
+    siteContainer.style.height = window.innerHeight + "px";
+});
 //on page load - display instructions if local storage is empty
